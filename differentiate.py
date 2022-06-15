49b90da40bc22b3dc580bb63d950b06364b19e41
Elston <elstuhnofficial@gmail.com>
2022-06-15 19:13:42 +0800

    all

---
+++ differentiate.py
@@ -0,0 +1,48 @@
+def differentiate(equation : str):
+    """
+    equation will be a polynomial e.g. 3x^2+4x+2 or 3/4x^3+2x^2+5x+1/2
+    equation will have to be simplified e.g. 4x^2+3x+2 instead of 8x^5/2x^3+3x^2/x+2 and 4x^2+3x+2 instead of 6x^2-2x^2+8x-5x+2
+    """
+    flag = False
+    result = ""
+    indflag = False
+    equation = equation.split("x")
+
+    for i in equation:
+        if i=="":
+            continue
+        if i[1:].isdigit() and flag:
+            if i[0] == "^":
+                evaluated = str(eval(f"{coeff}*{i[1:ind]}"))+f"x^{int(i[1:ind])-1}" if (int(i[1:ind])-2) else str(eval(f"{coeff}*{i[1:ind]}"))+"x"
+                if i != equation[1] and evaluated[0] != "-":
+                    evaluated = "+" + evaluated
+                result += evaluated
+            else:
+                result += coeff if coeff[0] == "-" else "+"+coeff
+            
+        elif not flag:
+            coeff = i
+            flag = True 
+        else:
+            try:
+                ind = i.index("+")
+                indflag = True
+            except:
+                ind = i.index("-")
+            evaluated = str(eval(f"{coeff}*{i[1:ind]}"))+f"x^{int(i[1:ind])-1}" if (int(i[1:ind])-2) else str(eval(f"{coeff}*{i[1:ind]}"))+"x"
+            if i != equation[1] and evaluated[0] != "-":
+                evaluated = "+" + evaluated
+            result+=evaluated
+            if not len(i[ind:])-1:
+                coeff = eval(f"0+{i[ind:]}1")
+            else:
+                coeff = i[ind+1:] if indflag else i[ind:]
+    
+    return result
+
+                 
+print(differentiate("3x^3+2x^5+6x"))
+# 8x^5+4x^4+2x^3+8x^2+2x+1
+# [8, ^5+4, ^4+2, ^3+8, ^2+2, +1]
+# 7x^3 + 8x^2
+# [7, ^3+8, ^2]
---
+++ main.py
@@ -0,0 +1,53 @@
+from differentiate import differentiate
+import sympy
+import functools
+
+@functools.cache
+def nrMethod(polynomial : str, points : int = 2, offset : int = 0, increment : int = 1, threshold : int = 0.1) -> list[str]:
+    """
+    newton raphson method\n
+    don't worry about sanitizing the input, the function sanitizes it for you\n
+    estimates the root of a polynomial using points points on a 2d graph with intervals of increment starting from offset units away from origin\n
+    offset is the amount of units away from the origin to start placing down points at intervals e.g. if offset is 4, points will start 4 units away from origin\n
+    increment states the spacing/distance between each point e.g. if increment is 0.3, each point will be 0.3 units apart\n 
+    threshold is the margin of error allowed from y=0
+    """
+    if points < 1:
+        raise Exception("Must have at least one point!")
+    fprime = differentiate(polynomial)
+    x = sympy.Symbol("x")
+    polynomial = "*x".join(polynomial.split("x"))
+    fprime = "*x".join(fprime.split("x"))
+    f = sympy.sympify(polynomial.replace("^", "**"))
+    fprime = sympy.sympify(fprime.replace("^", "**"))
+    f = sympy.lambdify(x, f)
+    fprime = sympy.lambdify(x, fprime)
+    deriv = 1
+    prev = 0
+    roots = []
+    higher = offset 
+    lower = -higher
+    value = offset
+    pointlist = [-value, value]
+    flag = 1
+    for i in range(points-2):
+        value += increment
+        flag = -1 if flag else 1
+        pointlist.append(flag * value)
+            
+    while pointlist:
+        dellist = []
+        
+        for i in range(len(pointlist)):
+            pointlist[i] -= (f(pointlist[i])/fprime(pointlist[i]))
+            if f(pointlist[i]) < threshold:
+                roots.append(pointlist[i])
+                dellist.append(i)
+        dellist.sort(reverse=True)
+        for i in dellist:
+            del pointlist[i]
+            
+    return set(roots) 
+
+test = nrMethod("2x^2+5x+3", 5, 2, 2)
+print(test)
